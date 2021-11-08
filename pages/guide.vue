8ce87a1173a52acf22116d10b75dfde73cade5e6
Def-aki0530 <aki990530@gmail.com>
2021-11-09 00:40:30 +0900

    ガイドページの作成

--- pages/guide.vue
+++ pages/guide.vue
@@ -15,8 +15,17 @@
         </div>
         <div class="guide-content">
           <div class="guide-label">使い方</div>
-          <div>絵が完成してから使用する場合</div>
-          <div>絵を描いている途中で使用する場合</div>
+          <p>1. 画像を選択ボタンからシステムで使用したいイラストを読み込んで表示することが出来ます．</p>
+          <img src="@/assets/img/guide1.png" alt="guide1">
+          <p>2. カラーパターンのセレクトボックスから色の生成パターンを選択できます(現状一種類のみ)．</p>
+          <p>3. 未設定時RGBのセレクトボックスから色の表示形式を変更できます．</p>
+          <p>4. スポイトアイコンからブラウザのカラーピッカーツールを開くことが出来ます．ブラウザのカラーピッカーツールのスポイト機能を使用して色を選択するか，数値を入力欄に入力して基本色(入力)を設定します．</p>
+          <img src="@/assets/img/guide2.png" alt="guide2">
+          <p>5. + ボタンで一度に実行する色の入力色の数を増やすことが出来ます．× ボタンで削除も可能です．</p>
+          <p>6. 実行ボタンを押すとベースカラーや影色，ハイライトが出力されます．</p>
+          <img src="@/assets/img/guide3.png" alt="guide3">
+          <p>7. 生成された色を使ってイラストを塗ってみましょう！ (↓使用例 )</p>
+          <img src="@/assets/img/guide4.png" alt="guide4">
         </div>
       </div>
     </div>
@@ -34,7 +43,7 @@ export default {};
   background: $backgroundColor;
 }
 
-.guide-container{
+.guide-container {
   width: 800px;
   color: $mainColor;
   margin: 0 auto;
@@ -46,11 +55,19 @@ export default {};
   }
 }
 
-.guide-content{
+.guide-content {
   margin: 0 0 30px;
+  p {
+    margin: 0 0 5px;
+  }
+  img {
+    width: 100%;
+    height: auto;
+    margin: 0 0 10px;
+  }
 }
 
-.guide-label{
+.guide-label {
   font-size: 1.25rem;
   margin: 0 0 10px;
 }
